30d3472bf55ecefa361b29691047b8276a3c2c38
Srikant Khuntia <177768330+Srikant066@users.noreply.github.com>
2025-09-12 17:57:01 +0530

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,45 +1,43 @@
-import React, { useState } from 'react'
-import Navbar from './components/Navbar'
-import { Route, Routes, useLocation } from 'react-router-dom'
-import Home from './pages/Home'
-import BikeDetails from './pages/BikeDetails'
-import Bikes from './pages/Bikes'
-import MyBookings from './pages/MyBookings'
-import Footer from './components/Footer'
-import Layout from './pages/owner/Layout'
-import Dashboard from './pages/owner/Dashboard'
-import AddBike from './pages/owner/AddBike'
-import ManageBikes from './pages/owner/ManageBikes'
-import ManageBooking from './pages/owner/ManageBooking'
-
-
+import React from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import BikeDetails from './pages/BikeDetails';
+import Bikes from './pages/Bikes';
+import MyBookings from './pages/MyBookings';
+import Login from './pages/Login';
+import Layout from './pages/owner/Layout';
+import Dashboard from './pages/owner/Dashboard';
+import AddBike from './pages/owner/AddBike';
+import ManageBikes from './pages/owner/ManageBikes';
+import ManageBooking from './pages/owner/ManageBooking';
 
 const App = () => {
-  const [showLogin,setShowLogin]=useState(false)
-  const isOwnerPath=useLocation().pathname.startsWith('/owner')
+  const location = useLocation();
+  const isOwnerPath = location.pathname.startsWith('/owner');
+
   return (
     <>
-      {!isOwnerPath && <Navbar setShowLogin={setShowLogin}/>}
+      {!isOwnerPath && <Navbar />}
 
-      
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/bike_details/:id' element={<BikeDetails/>} />
-        <Route path='/bikes' element={<Bikes/>} />
-        <Route path='/my-bookings' element={<MyBookings/>} />
-        <Route path='/owner' element={<Layout/>}>
-          <Route index element={<Dashboard/>}/>
-          <Route path='add-bike' element={<AddBike/>}/>
-          <Route path='manage-bikes' element={<ManageBikes/>}/>
-          <Route path='manage-bookings' element={<ManageBooking/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/bike_details/:id" element={<BikeDetails />} />
+        <Route path="/bikes" element={<Bikes />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/owner" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="add-bike" element={<AddBike />} />
+          <Route path="manage-bikes" element={<ManageBikes />} />
+          <Route path="manage-bookings" element={<ManageBooking />} />
         </Route>
-
       </Routes>
-      
 
-      {!isOwnerPath && <Footer/>}
+      {!isOwnerPath && <Footer />}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-)
+  </BrowserRouter>
+);
