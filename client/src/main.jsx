4efecbb1ece195ac9c0364c3c8909c4f8b1d4839
Souravshreechandan <154435201+Souravshreechandan@users.noreply.github.com>
2025-09-17 19:12:12 +0530

    add login

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import App from './App.jsx';
 import './index.css';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
 
-root.render(
+
+createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <>
+      <App />
+    </>
   </BrowserRouter>
 );
